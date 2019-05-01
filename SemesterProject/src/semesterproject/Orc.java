a8d562495379fa2476b8ba8ac6f3dba1b577e3f7
Riley Tucker <39104746+RileyT@users.noreply.github.com>
2019-05-01 16:34:00 -0400

    Update Orc.java

--- SemesterProject/src/semesterproject/Orc.java
+++ SemesterProject/src/semesterproject/Orc.java
@@ -6,7 +6,8 @@
 
  *  Version:     1.0
  *  Date:        4/5/2019
- *  Description: 
+ *  Description: This class calculates the constitution, strength, and dexterity 
+                 scores for an Orc enemy.
  *  Assignment:  SDEV265  System - Software Analysis and Projects
  * 
  */
@@ -16,6 +17,7 @@ package semesterproject;
 public class Orc extends FantasyRace {
     
     // variables
+	// between 4-23, 5-24, 3-22
     int constituton = (int) ((Math.random()*((20-1)+1))+1) + 3;
     int strength = (int) ((Math.random()*((20-1)+1))+1) + 4;
     int dexterity = (int) ((Math.random()*((20-1)+1))+1) + 2;
