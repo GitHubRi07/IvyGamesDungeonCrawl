60e1ef79b51ac4b5b1cbf0f99e9034abd2a13a59
Riley Tucker <39104746+RileyT@users.noreply.github.com>
2019-05-01 15:53:47 -0400

    Add files via upload

---
+++ SemesterProject/src/semesterproject/AreaDefinition.java
@@ -0,0 +1,112 @@
+/**
+ *  Application: IvyGames_SoftwareProduct
+ *  Description: Take user through a 'choose your own adventure' style story.  
+ *  Title:       SoftwareProduct
+ *  Authors:       Riley Tucker,  Aiden Hutton,  Karen Stackhouse,  Christopher Fields
+
+ *  Version:     1.0
+ *  Date:        4/5/2019
+ *  Description: 
+ *  Assignment:  SDEV265  System - Software Analysis and Projects
+ * 
+ */
+package semesterproject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AreaDefinition {
+    
+    // declare global variables
+    private static String[] areaFirstMessage = new String[26];
+    private static String[] areaMessage = new String[26];
+    private static File areaFirstDescriptionFilePath;
+    private static File areaDescriptionFilePath;
+    private static int fileLineIndex;
+    private static int firstFileLineIndex;
+    
+    
+    // getters	
+    public static String getFirstMessage(int roomNum) {
+        return areaFirstMessage[roomNum];
+    }
+    public static String getAreaMessage(int roomNum) {
+        return areaMessage[roomNum];
+    }
+    
+    // setters
+    
+    
+    
+    // setup the area definitions for each room in the game.
+    public static void setupAreaDefinitions() {
+        System.out.println("setupAreaDefinition() -- Activate function to read area definitions from file");
+        fileLineIndex = 0;
+        firstFileLineIndex = 0;
+        try {
+            // set area description files
+            areaFirstDescriptionFilePath = new File("src/semesterProject/firstAreaDescriptions.txt");
+            areaDescriptionFilePath = new File("src/semesterProject/areaDescriptions.txt");
+        	System.out.println(areaFirstDescriptionFilePath);
+        	System.out.println(areaDescriptionFilePath);
+            
+            // create a scanner for the descrition files
+            Scanner firstDescriptionscanner = new Scanner(areaFirstDescriptionFilePath);
+            Scanner descriptionScanner = new Scanner(areaDescriptionFilePath);
+            System.out.println("\n\nFirst Time Area Descriptions.");
+            while (firstDescriptionscanner.hasNext()) {
+                String message = firstDescriptionscanner.nextLine();
+                areaFirstMessage[firstFileLineIndex] = message;
+                firstFileLineIndex++;
+                System.out.println(firstFileLineIndex + "Line Read From First Description File.");
+            }
+            System.out.println("\n\nArea Descriptions.");
+            while (descriptionScanner.hasNext()) {
+                String message = descriptionScanner.nextLine();
+                areaMessage[fileLineIndex] = message;
+                fileLineIndex++;
+                System.out.println(fileLineIndex + "Line Read From Description File.");
+            }
+            
+            // close files to complete file read
+            firstDescriptionscanner.close();
+            descriptionScanner.close();
+        } catch (IOException ex) {
+            if ((!areaFirstDescriptionFilePath.exists()) || (areaFirstDescriptionFilePath.isDirectory())) {
+                System.out.println("The file at:" + areaFirstDescriptionFilePath + " does exist or can not be found.");
+            } else if (!areaDescriptionFilePath.exists() || (areaDescriptionFilePath.isDirectory())) {
+                System.out.println("The file at:" + areaDescriptionFilePath + " does exist or can not be found.");
+            } else {
+                System.out.println("\nException: " + ex + "\n");
+                ex.printStackTrace();
+            }
+        }
+    }
+    
+    
+    // set and display area message
+    public static void areaMessage(int roomNum) {
+        if (!Areas.getBeenTo(roomNum)) {
+            // first time message
+            SemesterProject.storyOutputTF.appendText(getFirstMessage(roomNum));
+        } else {
+            // regular message
+            SemesterProject.storyOutputTF.appendText(getAreaMessage(roomNum));
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
