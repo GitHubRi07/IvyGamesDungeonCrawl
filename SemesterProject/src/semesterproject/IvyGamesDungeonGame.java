5c8f2280ef120942b08bc326d96dd50a1c32810f
Riley Tucker <39104746+RileyT@users.noreply.github.com>
2019-05-01 04:04:41 -0400

    Update and rename IvyTechDungeonGame.java to IvyGamesDungeonGame.java

---
+++ SemesterProject/src/semesterproject/IvyGamesDungeonGame.java
@@ -0,0 +1,29 @@
+/**
+ *  Application: IvyGames_SoftwareProduct
+ *  Description: Take user through a 'choose your own adventure' style story.  
+ *  Title:		 SoftwareProduct
+ *  Authors:	 Riley Tucker,  Aiden Hutton,  Karen Stackhouse,  Christopher Fields
+
+ *  Version:     1.0
+ *  Date:        4/5/2019
+ *  Description: IvyTech Dungeon Game is a game where the player finds a suspicious cave entrance, then enter the dungeon.
+ *  			 The dungeon contains enemies and treasures. The player fights their way through many dungeon rooms
+ *  			 and collects upgrades along the way. The player wins the game by making their way to the boss 
+ *  			 room and defeating that boss.
+ *  Assignment:  SDEV265  System - Software Analysis and Projects
+ * 
+ */
+package semesterproject;
+
+
+public class IvyGamesDungeonGame { 
+    
+    // start IvyGames Dungeon Game!
+    public static void startGame() { 
+        // set starting area
+        Areas.setArea(0);
+        Areas.activateArea();
+        
+        
+    }
+}
