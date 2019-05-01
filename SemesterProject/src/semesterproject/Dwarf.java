cc0554ecdc31bca7a2cb1d20921ca4e9f5490371
Riley Tucker <39104746+RileyT@users.noreply.github.com>
2019-05-01 16:34:05 -0400

    Update Dwarf.java

--- SemesterProject/src/semesterproject/Dwarf.java
+++ SemesterProject/src/semesterproject/Dwarf.java
@@ -6,7 +6,8 @@
 
  *  Version:     1.0
  *  Date:        4/5/2019
- *  Description: 
+ *  Description: This class calculates the constitution, strength, and dexterity 
+                 scores for a Dwarf character.
  *  Assignment:  SDEV265  System - Software Analysis and Projects
  * 
  */
@@ -16,6 +17,7 @@ package semesterproject;
 public class Dwarf extends FantasyRace {
     
     // variables
+	// between 5-24, 4-23, 3-22
     int constituton = (int) ((Math.random()*((20-1)+1))+1) + 4;
     int strength = (int) ((Math.random()*((20-1)+1))+1) + 3;
     int dexterity = (int) ((Math.random()*((20-1)+1))+1) + 2;
