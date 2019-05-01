3c4ab69ea6ab47015f3482b43c216dee8648040b
Riley Tucker <39104746+RileyT@users.noreply.github.com>
2019-05-01 16:34:04 -0400

    Update Elf.java

--- SemesterProject/src/semesterproject/Elf.java
+++ SemesterProject/src/semesterproject/Elf.java
@@ -6,7 +6,8 @@
 
  *  Version:     1.0
  *  Date:        4/5/2019
- *  Description: 
+ *  Description: This class calculates the constitution, strength, and dexterity 
+                 scores for an Elf character.
  *  Assignment:  SDEV265  System - Software Analysis and Projects
  * 
  */
@@ -16,6 +17,7 @@ package semesterproject;
 public class Elf extends FantasyRace {
     
     // variables
+	// between 3-22, 4-23, 5-24
     int constituton = (int) ((Math.random()*((20-1)+1))+1) + 2;
     int strength = (int) ((Math.random()*((20-1)+1))+1) + 3;
     int dexterity = (int) ((Math.random()*((20-1)+1))+1) + 4;
