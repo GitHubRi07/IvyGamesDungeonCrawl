588f8ca4f339d26be2d96f6011e09312b47ddf5f
Riley Tucker <39104746+RileyT@users.noreply.github.com>
2019-05-01 04:34:42 -0400

    Update SemesterProject.java

--- SemesterProject/src/semesterproject/SemesterProject.java
+++ SemesterProject/src/semesterproject/SemesterProject.java
@@ -1,13 +1,15 @@
-// IvyTech Dungeon Game
-/* 
-* IvyTech Dungeon Game is a game where the player finds a suspicious cave entrance, then enter the dungeon.
-* The dungeon contains enemies and treasures. The player fights their way through many dungeon rooms
-* and collects upgrades along the way. The player wins the game by making their way to the boss 
-* room and defeating that boss.
-*/
-// Riley Tucker, Aiden Hutton
-// Karen Stackhouse, Christopher Fields
-// Created On 4/18/2019
+/**
+ *  Application: IvyGames_SoftwareProduct
+ *  Description: Take user through a 'choose your own adventure' style story.  
+ *  Title:       SoftwareProduct
+ *  Authors:       Riley Tucker,  Aiden Hutton,  Karen Stackhouse,  Christopher Fields
+
+ *  Version:     1.0
+ *  Date:        4/5/2019
+ *  Description: 
+ *  Assignment:  SDEV265  System - Software Analysis and Projects
+ * 
+ */
 package semesterproject;
 
 
@@ -238,7 +240,7 @@ public class SemesterProject extends Application {
             if (!gameStarted) { 
                 if (key.equals(KeyCode.ENTER)) {
                     storyOutputTF.appendText("\n\n");
-                    IvyTechDungeonGame.startGame();
+                    IvyGamesDungeonGame.startGame();
                     raceSelected = true;
                     gameStarted = true;
                 }
