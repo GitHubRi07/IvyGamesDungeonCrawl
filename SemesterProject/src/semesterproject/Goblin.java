4f5214c00bb41de48ffd4122b9651199527cab5b
Riley Tucker <39104746+RileyT@users.noreply.github.com>
2019-05-01 16:34:01 -0400

    Update Goblin.java

--- SemesterProject/src/semesterproject/Goblin.java
+++ SemesterProject/src/semesterproject/Goblin.java
@@ -6,7 +6,8 @@
 
  *  Version:     1.0
  *  Date:        4/5/2019
- *  Description: 
+ *  Description: This class calculates the constitution, strength, and dexterity 
+                 scores for a Goblin enemy.
  *  Assignment:  SDEV265  System - Software Analysis and Projects
  * 
  */
@@ -16,6 +17,7 @@ package semesterproject;
 public class Goblin extends FantasyRace {
     
     // variables
+	// between 3-22, 3-22, 6-25
     int constituton = (int) ((Math.random()*((20-1)+1))+1) + 2;
     int strength = (int) ((Math.random()*((20-1)+1))+1) + 2;
     int dexterity = (int) ((Math.random()*((20-1)+1))+1) + 5;
