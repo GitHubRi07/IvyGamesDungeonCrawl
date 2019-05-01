7686ef3ba79d7e3bb708a08f25f238b9ba9e5a63
Riley Tucker <39104746+RileyT@users.noreply.github.com>
2019-05-01 16:33:57 -0400

    Update Ogre.java

--- SemesterProject/src/semesterproject/Ogre.java
+++ SemesterProject/src/semesterproject/Ogre.java
@@ -6,7 +6,8 @@
 
  *  Version:     1.0
  *  Date:        4/5/2019
- *  Description: 
+ *  Description: This class calculates the constitution, strength, and dexterity 
+                 scores for an Ogre enemy.
  *  Assignment:  SDEV265  System - Software Analysis and Projects
  * 
  */
@@ -16,6 +17,7 @@ package semesterproject;
 public class Ogre extends FantasyRace {
     
     // variables
+	// between 5-24, 6-25, 2-21
     int constituton = (int) ((Math.random()*((20-1)+1))+1) + 4;
     // this is my swamp now, donkey
     int strength = (int) ((Math.random()*((20-1)+1))+1) + 5;
